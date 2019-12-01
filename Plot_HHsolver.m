0db6061cdd5d79f506345be2942a56f0b33dc9bb
Ryan Lisowski <58278745+Ryski2@users.noreply.github.com>
2019-12-01 15:49:12 -0800

    Add files via upload

---
+++ Plot_HHsolver.m
@@ -0,0 +1,33 @@
+close all; clear all; clc;
+trials = 100;
+time = 0:0.01:100;
+FractionN = zeros(trials,length(time));
+FractionK = zeros(trials,length(time));
+VOut = zeros(trials,length(time));
+VOut2 = zeros(trials,length(time));
+Subunit = zeros(1,trials);
+VClamp = zeros(1,trials);
+System = zeros(1,trials);
+
+%for i = 1:trials;
+    %E = StochasticHH_func(time, @(t) 0, [], 100,'ODE');
+    
+    %S = StochasticHH_func([0:0.01:100], @(t) 0, [1], 100,'Subunit');
+    %V = StochasticHH_func([0:0.01:100], @(t) 0, [1], 100,'VClamp');
+    FLS = StochasticHH_funcb(time, @(t) 0, [1], 10,'Subunit');
+    %FractionN(i,:) = FLS(:,3);
+    %FractionK(i,:) = FLS(:,4);
+    %VOut(i,:) = FLS(:,2);
+    %VOut2(i,:) = FLS(:,8);
+%end
+
+hold on;
+%figure(1)
+%plot(FLS(:,1), FLS(:,3)), title('Fraction of Open Na channels vs. Time')
+%figure(2)
+%plot(FLS(:,1), FLS(:,4)), title('Fraction of Open K channels vs. Time')
+figure(1)
+plot(FLS(:,1), FLS(:,2)), title('Voltage vs. Time')
+plot(FLS(:,1), FLS(:,9)), title('Voltage vs. Time 2')
+figure(5)
+plot(FLS(:,1), FLS(:,16)), title('Voltage vs. Time 3')
