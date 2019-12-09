034d0c8915a9ed6cd295c39a98e2defaae9b9b0a
Katie Rupp <48692614+katierupp@users.noreply.github.com>
2019-12-08 19:55:09 -0800

    update

--- mean_isi_coupled.m
+++ mean_isi_coupled.m
@@ -1,22 +1,24 @@
-% returns the mean interspike intervals for two coupled neurons given 
+% computes the mean interspike intervals for two coupled neurons given 
 % the time, type of noise, the number of trials, and the coupling strength
 % and noise intensity
-function [avg_mean_isi, avg_mean_isi2] = mean_isi_coupled(t, noise, ntrials, coupling_strength, noise_intensity)
+function [avg_mean_isi, avg_mean_isi2] = mean_isi_coupled(t, noise, ntrials, n_neurons, coupling_strength, noise_intensity)
     
     mean_isi_vec = zeros(1, ntrials);
-    mean_isi_vec2 = zeros(1, ntrials);  
+    mean_isi_vec2 = zeros(1, ntrials);
+    
     for j = 1:ntrials 
         
-        Y = coupled_HH_func(t, @(t) 10, 0.5, 100, noise, coupling_strength, noise_intensity);
+        % Y = coupled_HH_func(t, @(t) 10, 0.5, 100, noise, coupling_strength, noise_intensity);
+        Y = noise_coupling(n_neurons, coupling_strength, noise_intensity, t, @(t) 10, 10, 100, noise);
         
-        time = Y(:, 1);
-        voltage = Y(:, 2);
-        mean_isi = isi_coupled(time, voltage);
+        time = Y.t;
+        voltage = Y.V(:,1);
+        [mean_isi, spike_times] = isi_coupled(time, voltage);
         mean_isi_vec(j) = mean_isi;
                 
-        time2 = Y(:,8);
-        voltage2 = Y(:,9);
-        mean_isi2 = isi_coupled(time2, voltage2);
+        time2 = Y.t;
+        voltage2 = Y.V(:,2);
+        [mean_isi2, spike_times2] = isi_coupled(time2, voltage2);
         mean_isi_vec2(j) = mean_isi2;
 
     end
