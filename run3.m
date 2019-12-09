c24165235210c9f9d3536202a821f201eae63754
Katie Rupp <48692614+katierupp@users.noreply.github.com>
2019-12-09 12:54:00 -0800

    update

--- run3.m
+++ run3.m
@@ -1,12 +1,13 @@
 % run 3
+
 % mean phase offset for a range of coupling strengths for each  
 % noise model 
 
 n_neurons = 2;
-ntrials = 100;
+ntrials = 1;
 noise_intensity = 1;
 coupling_strength = 0:0.1:1;
-t = [0:0.01:300];
+t = [0:0.01:50];
 noise = {'Subunit', 'FoxLuSystemSize', 'VClamp', 'MarkovChain', 'ODE'};
 
 mdp_su = zeros(1, length(ntrials));
@@ -31,11 +32,11 @@ for i = 1:length(coupling_strength)
         [delta_phi_ode, lengths_ode] = multiple_phase_offsets(t, n_neurons, kappa, noise_intensity, noise{4});
         [delta_phi_mc, lengths_mc] = multiple_phase_offsets(t, n_neurons, kappa, noise_intensity, noise{5});
         
-        mdp_su(j) = mean(delta_phi_su);
-        mdp_cd(j) = mean(delta_phi_cd);
-        mdp_vc(j) = mean(delta_phi_vc);
-        mdp_ode(j) = mean(delta_phi_ode);
-        mdp_mc(j) = mean(delta_phi_mc);
+        mdp_su(j) = mean(dist_from_asynchrony(delta_phi_su(1:lengths_su, :)));
+        mdp_cd(j) = mean(dist_from_asynchrony(delta_phi_cd(1:lengths_cd, :)));
+        mdp_vc(j) = mean(dist_from_asynchrony(delta_phi_vc(1:lengths_vc, :)));
+        mdp_ode(j) = mean(dist_from_asynchrony(delta_phi_ode(1:lengths_ode, :)));
+        mdp_mc(j) = mean(dist_from_asynchrony(delta_phi_mc(1:lengths_mc, :)));
 
     end
     mdp_su_means(i) = mean(mdp_su);
